930d4fef5980b07f30722051a60b260840f4a2bb
William Pereira <billpereira@MacBook-Pro-de-William.local>
2020-05-05 10:39:36 -0300

    RACF Route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,13 @@
 const {Router} = require('express');
 const AuthController = require('./controllers/AuthController');
+const RacfController = require('./controllers/RacfController');
 
 const routes = Router()
 
 routes.post("/api/login",AuthController.login)
+
+routes.post("/api/user/connect",RacfController.connectUser)
+routes.post("/api/user/remove",RacfController.removeConnection)
+routes.get("/api/user/list",RacfController.listUser)
 
 module.exports = routes
